50b5a7384bb6aaabdff602d24242ecffa6ec2c20
erica-jeong <wjddbwjd1205@gmail.com>
2024-11-29 16:12:14 +0900

    feat: 입력받은 자동차를 객체로 만들어서 저장하는 기능 추가

---
+++ src/Car.js
@@ -0,0 +1,11 @@
+class Car {
+  // #name;
+  // #goCount;
+
+  constructor(name) {
+    this.name = name;
+    this.goCount = 0;
+  }
+}
+
+export default Car;
--- src/CarRacingManager.js
+++ src/CarRacingManager.js
@@ -1,6 +1,7 @@
 import InputView from './InputView.js'
 import OutputView from './OutputView.js'
 import Validate from './Validate.js'
+import Car from './Car.js'
 
 class CarRacingManager {
   #inputView
@@ -17,6 +18,8 @@ class CarRacingManager {
     try {
       // 1. 자동차 이름을 입력 받는 기능
       const carNames = await this.#getCarsName();
+      const cars = this.#makeCars(carNames);
+      
       // 2. 시도할 횟수를 입력 받는 기능
       const attemptCount = await this.#getAtemptCount();
     } catch (error) {
@@ -48,6 +51,15 @@ class CarRacingManager {
         this.#outputView.printErrorMessage(error.message);
       }
     }
+  }
+
+  #makeCars(carNames) {
+    const cars = [];
+    carNames.forEach(name => {
+      const car = new Car(name);
+      cars.push(car);
+    });
+    return cars;
   }
 }
 
